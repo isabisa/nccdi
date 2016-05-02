a95701d7ee07207aab70453132971ba1bf0fafa8
isabisa <alisa.r.herr@gmail.com>
2016-05-02 08:37:36 -0400

    Fix resource page. Update plugins.

--- wp-content/themes/nccdi/templates/layouts/block-resource.php
+++ wp-content/themes/nccdi/templates/layouts/block-resource.php
@@ -5,7 +5,7 @@ use Roots\Sage\Assets;
 ?>
 <div class="grid flex">
   <?php while (have_posts()) : the_post(); ?>
-    <article <?php post_class('block-post ' . implode($classes, ' ')); ?>>
+    <article <?php post_class('block-post'); ?>>
       <a class="mega-link" href="<?php the_permalink(); ?>"></a>
       <header class="entry-header">
         <h4 class="post-title"><?php the_title(); ?></h4>
