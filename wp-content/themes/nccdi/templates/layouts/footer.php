9917d1bd7008e74b9729160e96ec7d04a055e4a1
Alisa Herr <alisa@unitymakes.us>
2017-03-17 14:32:02 -0400

    Footer and style updates to coordinate with NCHC redesign

--- wp-content/themes/nccdi/templates/layouts/footer.php
+++ wp-content/themes/nccdi/templates/layouts/footer.php
@@ -27,9 +27,9 @@ use Roots\Sage\Nav;
 
 <footer class="global-footer">
   <div class="container">
-    <div class="col-sm-6 col-md-4">
+    <div class="col-sm-6 col-md-4 footer-logos">
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo Assets\asset_path('images/nccdi-logo-footer.jpg'); ?>" /></a>
-      <br /><br />
+      <br />
       <a href="http://nchousing.org" target="_blank"><img src="<?php echo Assets\asset_path('images/nchc-logo-footer.jpg'); ?>" /></a>
     </div>
 
@@ -38,7 +38,7 @@ use Roots\Sage\Nav;
         Raleigh, NC 27609</address>
       <p><tel>919.828.5655</tel> | <a href="/contact/">Contact Us</a></p>
       <br />
-      <p><a class="btn btn-skew btn-teal" data-toggle="modal" data-target="#emailSignupModal">Subscribe</a></p>
+      <p><a class="btn btn-skew btn-lg btn-teal" data-toggle="modal" data-target="#emailSignupModal">Subscribe</a></p>
     </div>
 
     <div class="col-sm-6 col-sm-push-6 col-md-push-0 col-md-3">
@@ -67,29 +67,6 @@ use Roots\Sage\Nav;
           <a class="icon-flickr" href="http://www.flickr.com/photos/ncinitiative/" target="_blank" rel="nofollow"></a>
         </div>
       </div>
-    </div>
-  </div>
-</footer>
-
-<footer class="global-footer">
-  <div class="striped-line"></div>
-
-  <div class="container">
-    <div class="text-center">
-      <p class="h4">
-        Get the latest news
-        <a class="btn btn-skew btn-teal" data-toggle="modal" data-target="#emailSignupModal">Subscribe</a>
-        <a class="icon-facebook" href="http://www.facebook.com/ncinitiative5800" target="_blank" rel="nofollow"></a>
-        <a class="icon-twitter" href="http://twitter.com/ncinitiative" target="_blank" rel="nofollow"></a>
-        <a class="icon-flickr" href="http://www.flickr.com/photos/ncinitiative/" target="_blank" rel="nofollow"></a>
-      </p>
-
-      <p>Copyright &copy; <?php echo date('Y'); ?> &nbsp;|&nbsp; NC Community Development Initiative &nbsp;|&nbsp; 919.828.5655 &nbsp;|&nbsp; <a href="mailto:<?php echo antispambot('info@ncinitiative.org'); ?>"><?php echo antispambot('info@ncinitiative.org'); ?></a></p>
-
-      <p>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo Assets\asset_path('images/nccdi-logo-footer.jpg'); ?>" /></a>
-        <a href="http://nchousing.org" target="_blank"><img src="<?php echo Assets\asset_path('images/nchc-logo-footer.jpg'); ?>" /></a>
-      </p>
     </div>
   </div>
 </footer>
